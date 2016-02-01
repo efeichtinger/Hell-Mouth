8949d62e8ff29efb88a39a3e722699add79dea6c
Erin <efeichtinger@mail.usf.edu>
2016-01-31 19:56:05 -0500

    Final script

--- Module3.R
+++ Module3.R
@@ -12,3 +12,36 @@ NBC_politicalpollresults <- c(12,75,43,19,1,21,19)
 pres.cand <- cbind(Name, ABC_politicalpollresults, NBC_politicalpollresults)
 pres.cand
 
+pres.cand <- as.data.frame(pres.cand)
+mean(pres.cand, na.rm=TRUE)
+mean(pres.cand[,2:3])
+summary(pres.cand)
+
+#I can look at each candidate's polling result by indexing
+Jeb <- pres.cand[1,1:3]
+Donald <- pres.cand[2,1:3]
+Ted <- pres.cand[3,1:3]
+Marco <- pres.cand[4,1:3]
+Carly <- pres.cand[5,1:3]
+Hillary <- pres.cand[6,1:3]
+Bernie <- pres.cand[7,1:3]
+
+ggplot(pres.cand, aes(pres.cand$Name, y="Polling results", color = "Media Outlet")) + 
+  geom_point(aes(y = pres.cand$ABC_politicalpollresults , col = "ABC")) + 
+  geom_point(aes(y = pres.cand$NBC_politicalpollresults, col = "NBC")) + 
+  labs(x = "Candidate", y = "Polling Results")   +
+theme(
+    axis.title=element_text(face="bold", size = "12", color="black"))
+  
+
+
+
+
+
+
+
+
+
+
+
+

47650b2a6502f6fe96621c3156c07f5959bfc65b
Erin <efeichtinger@mail.usf.edu>
2016-02-14 19:03:10 -0500

    Cleaned up code, 'final'

--- Module5.R
+++ Module5.R
@@ -22,24 +22,31 @@ fd <- c(0,1,0,1,0,1,0,1,1,1)
 df1 <- data.frame(freq, bloodp, first, second, fdec)
 df2 <- data.frame(freq, bloodp, fst, scd, fd)
 
+hist(df2$freq, main = "", xlab = "Frequency of Hospital Visits in Past Twelve Months",
+     ylab = "Number of Cases", pin = c(5,5))
+hist(df2$bloodp, main = "", xlab = "Blood Pressure", pin = c(5,5))
+
 #brute force approach
 bp1 <- c(59,109,78,205,103,87,32,42,176,135)
+frqq <- c(0.2,0.6,0.3,0.4,0.6,0.3,0.4,0.4,0.2,0.9)
 fstt <- c(0,0,0,0,1,1,1,1,1,NA)
 d <- data.frame(bp1,fstt)
-#add in labels and stuff
-boxplot(unstack(d))
-
-
-ID <- c(1,1,1,2,2,2,3,3,3,4,4,4,5,5,5,6,6,6,7,7,7,8,8,8,9,9,9,10,10,10)
-f <- c(0.6,0.6,0.6,0.3,0.3,0.3,0.4,0.4,0.4,0.4,0.4,0.4,0.2,0.2,0.2,0.6,0.6,0.6,
-       0.3,0.3,0.3,0.4,0.4,0.4,0.9,0.9,0.9,0.2,0.2,0.2)
-bp <- c(103,103,103,87,87,87,32,32,32,42,42,42,59,59,59,109,109,109,78,
-        78,78,205,205,205,135,135,135,176,176,176)
-test.ID <- c(1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3)
-test.res <- c(0,0,0,0,0,1,0,1,0,0,1,1,1,0,0,1,0,1,1,1,0,1,1,1,1,1,1,NA,1,1)
-d <- data.frame(ID,f,bp,test.ID,test.res)
-arrange(d, test.res)
-
+dd <- data.frame(frqq, fstt)
+#blood pressure distribution between groups according to the first test 
+boxplot(unstack(d), xlab = "First Test Result", ylab = "Blood Pressure",
+        pin = c(5,5))
+boxplot(unstack(dd), xlab = "First Test Result", 
+      ylab = "Frequency of Visits Past 12 Months", pin = c(5,5))
+sctt <- c(0,0,0,0,1,1,1,1,1,1)
+bp2 <- c(103,87,59,109,32,42,78,205,135,176)
+ddd <- data.frame(bp2,sctt)
+boxplot(unstack(ddd), xlab = "Second Test Result", ylab = "Blood Pressure",
+        pin = c(5,5))
+thdtt <- c(0,0,0,0,1,1,1,1,1,1)
+bp3 <- c(103,32,59,78,87,42,109,205,135,176)
+dddd <- data.frame(bp3, thdtt)
+boxplot(unstack(dddd), xlab = "Third Test Result", ylab = "Blodd Pressure",
+        pin = c(5,5))
 #some problems using stack and unstack with the data frames
 #errors involving using factors, it ignores them 
 #probably have to use reshape
